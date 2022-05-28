8c21780e439a2d722857cc9ccf8b7352845d715e
Sunny <sunnykr3913@gmail.com>
2022-05-28 11:50:54 +0530

    Initial commit

--- C++ Course/mergearray.cpp
+++ C++ Course/mergearray.cpp
@@ -1,18 +1,89 @@
-
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-struct stud1
-{
-    int roll;
-    int roll2;
-} s1,s2, *ptr;
 int main()
 {
-    s1.roll= 9;
-    cout << s1.roll;
-    *ptr.&roll = 7;
-
+    int t;
+    cout << "Enter a number = \n";
+    cin >> t;
+    while (t--)
+    {
+        int n, c = 0;
+        cin >> n;
+        string str[n];
+        cin >> str[n];
+        cout << str[3];
+        for (int i = 0; i < n + 1; i++)
+        {
+            if (str[i] != str[n - i - 1])
+            {
+                c++;
+            }
+        }
+        cout << c;
+    }
 }
+// #include <iostream>
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int t;
+//     cout << "Enter a number = \n";
+//     cin >> t;
+//     while (t--)
+//     {
+//         int n;
+//         cin >> n;
+//         int a[n];
+//         for (int i = 0; i < n; i++)
+//         {
+//             cin >> a[i];
+//         }
+//         int c = 0;
+//         for (int i = 0; i < n; i++)
+//         {
+//             for (int j = i; j < n; j++)
+//             {
+//                 for (int k = i; k <= j; k++)
+//                 {
+//                     if (a[k] == j - i + 1)
+//                     {
+//                         c++;
+//                         break;
+//                     }
+//                 }
+//             }
+//         }
+//         cout << c << endl;
+//     }
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     float n;
+//     cout << "Enter a number to convert Decimal to Binary : \n";
+//     cin >> n;
+//     int t = 1, p = 0;
+//     while (t <= n)
+//     {
+//         t *= 2;
+//     }
+//     t /= 2;
+//     while (t > 0)
+//     {
+//         float last = n / t;
+//         n = n - (t * last);
+//         t /= 2;
+//         p = p * 10 + last;
+//     }
+//     cout << p;
+
+//     return 0;
+// }
 
 // #include <iostream>
 // using namespace std;
@@ -55,5 +126,5 @@ int main()
 //     {
 //         cout << a[i] << " ";
 //     }
-    
+
 // }
